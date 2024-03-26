a40290522ed57744eefa51024411184cb5b4969a
abubaker khaled <abubakr_20210011@fci.helwan.edu.eg>
2024-03-26 23:44:25 +0200

    downloading a playlist...

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-from pytube import YouTube
+from pytube import YouTube, Playlist
 import os
-from pathlib import Path
-
-
-
-video_link = input("Video link: ")
 
 # Write a regular expression for the link 
+# -------Video--------
 # https://youtu.be/EKDSKynq0xs?list=PLgRZV-Axn6g7n6dmtVfQVnS75ZvKOZ8y9
 # https://www.youtube.com/watch?v=EKDSKynq0xs&list=PLgRZV-Axn6g7n6dmtVfQVnS75ZvKOZ8y9&index=3&t=1s
+# https://www.youtube.com/watch?v=vXukSWdjad4
 
+# -------Playlist-----
+# https://www.youtube.com/watch?v=41qgdwd3zAg&list=PLS1QulWo1RIaJECMeUT4LFwJ-ghgoSH6n
 
 def on_progress(stream, chunk, bytes_remaining):
     """Callback function"""
@@ -18,17 +17,52 @@ def on_progress(stream, chunk, bytes_remaining):
     pct_completed = bytes_downloaded / total_size * 100
     print(f"Status: {round(pct_completed, 2)}%")
 
-# if the regular expression is true then make a youtube object
-yt = YouTube(video_link,on_progress_callback=on_progress)
 
+choose = ''
+choose = input("if you want to download a Video press v or Playlist p: ")
 
-for stream in yt.streams.filter(progressive=True):
-    print(stream)
+if choose == ['v', 'V']:
     
-itag = input("Itag: ")
-    
-path = input("Path: ")
-path_to_download = os.path.join(os.path.expanduser("~"), path)
+    video_url = input("Video link: ")
+    # if the regular expression is true then make a youtube object
+    yt = YouTube(video_url,on_progress_callback=on_progress)
 
-stream = yt.streams.get_by_itag(itag)
-stream.download(path_to_download)
+
+    for stream in yt.streams.filter(progressive=True):
+        print(stream)
+        
+    itag = input("Itag: ")
+        
+    path = input("Path: ")
+    path_to_download = os.path.join(os.path.expanduser("~"), path)
+
+    stream = yt.streams.get_by_itag(itag)
+    stream.download(path_to_download)
+    
+else:
+    playlist_url = input("Playlist link: ")
+    
+    p = Playlist(playlist_url)
+    
+    print(f'Downloading: {p.title}')
+    
+    path = input("Path: ")
+    path_to_download = os.path.join(os.path.expanduser("~"), path)
+    
+    for video in p.videos:
+        
+        print(video.title)
+        
+        stream = video.streams
+        
+        for stream in video.streams.filter(progressive=True):
+            print(stream)    
+            
+        itag = input("Itag: ")
+
+        stream = video.streams.get_by_itag(itag)
+        stream.download(path_to_download)
+
+
+
+
