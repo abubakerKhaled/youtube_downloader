3ab1ba0bfb38f1b309cd58c2ac1bca879a188977
abubaker khaled <abubakr_20210011@fci.helwan.edu.eg>
2024-03-28 00:42:10 +0200

    Making more changes and updates, functions...

--- main.py
+++ main.py
@@ -1,68 +1,55 @@
 from pytube import YouTube, Playlist
 import os
+import re
 
-# Write a regular expression for the link 
-# -------Video--------
-# https://youtu.be/EKDSKynq0xs?list=PLgRZV-Axn6g7n6dmtVfQVnS75ZvKOZ8y9
-# https://www.youtube.com/watch?v=EKDSKynq0xs&list=PLgRZV-Axn6g7n6dmtVfQVnS75ZvKOZ8y9&index=3&t=1s
-# https://www.youtube.com/watch?v=vXukSWdjad4
+# Constants
+VIDEO_REGEX = r'(https?://)?(www\.)?(youtube\.com|youtu\.?be)/.+'
+DEFAULT_PATH = os.path.join(os.path.expanduser("~"), 'Downloads')
 
-# -------Playlist-----
-# https://www.youtube.com/watch?v=41qgdwd3zAg&list=PLS1QulWo1RIaJECMeUT4LFwJ-ghgoSH6n
-
+# Functions
 def on_progress(stream, chunk, bytes_remaining):
-    """Callback function"""
     total_size = stream.filesize
     bytes_downloaded = total_size - bytes_remaining
     pct_completed = bytes_downloaded / total_size * 100
     print(f"Status: {round(pct_completed, 2)}%")
 
+def get_validated_input(prompt, pattern=None):
+    user_input = input(prompt)
+    while pattern and not re.match(pattern, user_input):
+        print("Invalid input. Please try again.")
+        user_input = input(prompt)
+    return user_input
 
-choose = ''
-choose = input("if you want to download a Video press v or Playlist p: ")
+def construct_path():
+    path = input("Enter download directory path (leave empty for default): ").strip()
+    if not path:
+        return DEFAULT_PATH
+    else:
+        return os.path.join(os.path.expanduser("~"), path)
 
-if choose == 'v' or choose == 'V':
+def download_video(video_url, path_to_download):
+    video = YouTube(video_url, on_progress_callback=on_progress)
+    print(f'Video title: {video.title}')
+    stream = video.streams.filter(progressive=True).first()
+    stream.download(path_to_download, on_progress=on_progress)
+    print("Download completed.")
+
+def download_playlist(playlist_url, path_to_download):
+    playlist = Playlist(playlist_url)
+    print(f'Downloading Playlist: {playlist.title}')
+    for video in playlist.videos:
+        print(f'Downloading: {video.title}')
+        stream = video.streams.filter(progressive=True).first()
+        stream.download(path_to_download, on_progress=on_progress)
+    print("Download completed.")
+
+# Main Script
+if __name__ == "__main__":
+    choose = get_validated_input("Download a Video (v) or Playlist (p): ", pattern=r'[VvPp]')
+    url = get_validated_input("Enter the link: ", pattern=VIDEO_REGEX)
+    path_to_download = construct_path()
     
-    video_url = input("Video link: ")
-    # if the regular expression is true then make a youtube object
-    yt = YouTube(video_url,on_progress_callback=on_progress)
-
-
-    for stream in yt.streams.filter(progressive=True):
-        print(stream)
-        
-    itag = input("Itag: ")
-        
-    path = input("Path: ")
-    path_to_download = os.path.join(os.path.expanduser("~"), path)
-
-    stream = yt.streams.get_by_itag(itag)
-    stream.download(path_to_download)
-    
-else:
-    playlist_url = input("Playlist link: ")
-    
-    p = Playlist(playlist_url)
-    
-    print(f'Downloading: {p.title}')
-    
-    path = input("Path: ")
-    path_to_download = os.path.join(os.path.expanduser("~"), path)
-    
-    for video in p.videos:
-        
-        print(video.title)
-        
-        stream = video.streams
-        
-        for stream in video.streams.filter(progressive=True):
-            print(stream)    
-            
-        itag = input("Itag: ")
-
-        stream = video.streams.get_by_itag(itag)
-        stream.download(path_to_download)
-
-
-
-
+    if choose.lower() == 'v':
+        download_video(url, path_to_download)
+    else:
+        download_playlist(url, path_to_download)
